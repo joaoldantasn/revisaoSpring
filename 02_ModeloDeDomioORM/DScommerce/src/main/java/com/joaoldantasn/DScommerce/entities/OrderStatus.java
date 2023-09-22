11eab6e6947386a714b25d016eadba4bf553a7ca
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-09-22 01:00:46 -0300

    Projeto DScommerce instanciado

---
+++ 02_ModeloDeDomioORM/DScommerce/src/main/java/com/joaoldantasn/DScommerce/entities/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.joaoldantasn.DScommerce.entities;
+
+public enum OrderStatus {
+	
+	WAITING_PAYMENT,
+	PAID,
+	SHIPPED,
+	DELIVERED,
+	CANCELED
+}
---
+++ 02_ModeloDeDomioORM/DScommerce/src/test/java/com/joaoldantasn/DScommerce/DScommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoldantasn.DScommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DScommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
