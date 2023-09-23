583a92315ad3094e896e700c9e7c39781e96611f
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-09-23 16:21:31 -0300

    Equals e Hashcode

--- 02_ModeloDeDomioORM/DScommerce/src/main/java/com/joaoldantasn/DScommerce/entities/Order.java
+++ 02_ModeloDeDomioORM/DScommerce/src/main/java/com/joaoldantasn/DScommerce/entities/Order.java
@@ -3,6 +3,7 @@ package com.joaoldantasn.DScommerce.entities;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -101,6 +102,25 @@ public class Order {
 	//get products daquele order
 	public List<Product> getProducts(){
 		return items.stream().map(x -> x.getProduct()).toList();
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
 	}
 
 	
