8abc42d13f0196fbcb16ed26d194f9a8f327e706
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-09-23 21:32:44 -0300

    GET product by id

--- 03_ApiRest/DScommerce2/src/main/java/com/joaoldantasn/DScommerce/controllers/ProductController.java
+++ 03_ApiRest/DScommerce2/src/main/java/com/joaoldantasn/DScommerce/controllers/ProductController.java
@@ -1,26 +1,24 @@
 package com.joaoldantasn.DScommerce.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.joaoldantasn.DScommerce.entities.Product;
-import com.joaoldantasn.DScommerce.repositories.ProductRepository;
+import com.joaoldantasn.DScommerce.dto.ProductDTO;
+import com.joaoldantasn.DScommerce.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 	
 	@Autowired
-	private ProductRepository repository;
+	private ProductService service;
 	
-	@GetMapping
-	public String teste() {
-		Optional<Product> result = repository.findById(1L);
-		Product product = result.get();
-		return product.getName();
+	@GetMapping(value = "/{id}")
+	public ProductDTO findById(@PathVariable Long id) {
+		ProductDTO dto = service.findById(id);
+		return dto;
 	}
 }
