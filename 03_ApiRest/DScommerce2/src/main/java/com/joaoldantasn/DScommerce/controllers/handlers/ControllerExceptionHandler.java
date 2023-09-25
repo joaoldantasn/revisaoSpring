96e215e491ac55b889c3e82ba8870b36e83f1de7
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-09-24 22:06:52 -0300

    Tratando Excecao com reposta customizada

---
+++ 03_ApiRest/DScommerce2/src/main/java/com/joaoldantasn/DScommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.joaoldantasn.DScommerce.controllers.handlers;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.joaoldantasn.DScommerce.dto.CustomError;
+import com.joaoldantasn.DScommerce.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
